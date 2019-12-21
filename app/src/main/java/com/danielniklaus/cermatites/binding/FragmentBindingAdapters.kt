114542ef17e7bc7471255c57c5d29c8a347726b6
Daniel niklaus <daniel.niklaus16@gmail.com>
2019-12-21 07:32:24 +0700

    add placeholder in glide

--- app/src/main/java/com/danielniklaus/cermatites/binding/FragmentBindingAdapters.kt
+++ app/src/main/java/com/danielniklaus/cermatites/binding/FragmentBindingAdapters.kt
@@ -6,12 +6,14 @@ import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.danielniklaus.cermatites.R
 import javax.inject.Inject
 
 
 class FragmentBindingAdapters @Inject constructor(val fragment: Fragment) {
     @BindingAdapter(value = ["imageUrl", "imageRequestListener"], requireAll = false)
     fun bindImage(imageView: ImageView, url: String?, listener: RequestListener<Drawable?>?) {
-        Glide.with(fragment).load(url).listener(listener).into(imageView)
+        Glide.with(fragment).load(url).apply(RequestOptions().placeholder(R.drawable.ic_launcher_background)).listener(listener).into(imageView)
     }
 }
